fb24454955dcf03ab543d9f0af002d015ff7dcbd
RudekAlone <44509433+RudekAlone@users.noreply.github.com>
2023-03-27 21:14:32 +0200

    Add files via upload

--- testy/LSK/script.js
+++ testy/LSK/script.js
@@ -1,19 +1,51 @@
-
-
 let score = 0;
-let urlText = document.URL
-let urlTextArray = urlText.split("/");
-console.log(urlTextArray[urlTextArray.length - 1]);
+let ocena = 1;
+let odp = [
+    "D",
+    "A",
+    "A",
+    "B",
+    "D",
+    "C",
+    "C",
+    "C",
+    "D",
+    "D"
+];
 function checkAnswer(){
-    for(let i = 1; i<=3;i++)
+    score = 0;
+    for(let i = 1; i<=odp.length;i++)
     {
         
-    if(document.querySelector('input[name="test'+i+'"]:checked').value =='D')
-    {
-        score += 1;
+   
+        if(document.querySelector('input[name="test'+i+'"]:checked').value == odp[i-1])
+        {
+            score += 1;
+        }
+   
     }
+    console.log("shgdja");
+    console.log(odp.length)
+    if(score/odp.length<0.4){
+        ocena = 1;
     }
-    window.parent.document.getElementById("xp").innerText="jghg";
+    else if(score/odp.length<0.6){
+        ocena = 2;
+    }
+    else if(score/odp.length<0.7){
+        ocena = 3;
+    }
+    else if(score/odp.length<0.90){
+        ocena = 4;
+    }
+    else if(score/odp.length<0.96){
+        ocena = 5;
+    }
+    else{
+        ocena = 6;
+    }
+    
+    document.getElementById("questions-size").innerHTML= '<section class="center"><br><em>Wynik:</em>'+score+' / '+odp.length+' <br><h2>Ocena: '+ocena+'</h2></section>';
 }
 
 
